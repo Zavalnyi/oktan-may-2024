d095bed52f23f5e231f1f599efe73e63b342c417
zaval <zavalnyi.roman.v@g,ail.com>
2024-08-12 15:50:37 +0300

    complete lesson1

---
+++ Lessons/JS/Lesson1/lesson1.2.js
@@ -0,0 +1,6 @@
+let firstName = prompt('firstName');
+let middleName = prompt('middleName');
+let lastName =prompt('lastName');
+let person = firstName + ' ' + middleName + ' ' + lastName;
+
+console.log(person);
